4f15055ebc9033d7dca1cf9d25e8535332569a81
Dawoox <me@antoinecandelon.fr>
2023-06-04 13:32:45 +0200

    Update X360CEManagerSettings.cs

--- X360CEManagerSettings.cs
+++ X360CEManagerSettings.cs
@@ -2,10 +2,12 @@
 using Playnite.SDK.Data;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// ReSharper disable All
 namespace X360CEManager
 {
     public class X360CEManagerSettings : ObservableObject
@@ -13,7 +15,7 @@ namespace X360CEManager
         private bool _startWithPlayniteDefault = false;
         private bool _startWithAllGamesDefault = false;
         private bool _startWithSelectedGamesDefault = false;
-        private string _x360cePathDefault = string.Empty;
+        private string _x360cePathDefault = string.Empty; 
         
         public bool startWithPlaynite { get => _startWithPlayniteDefault; set => SetValue(ref _startWithPlayniteDefault, value); }
         public bool startWithAllGames { get => _startWithAllGamesDefault; set => SetValue(ref _startWithAllGamesDefault, value); }
@@ -21,6 +23,7 @@ namespace X360CEManager
         public string x360cePath { get => _x360cePathDefault; set => SetValue(ref _x360cePathDefault, value); }
     }
 
+    
     public class X360CEManagerSettingsViewModel : ObservableObject, ISettings
     {
         private readonly X360CEManager plugin;
@@ -30,59 +33,34 @@ namespace X360CEManager
         public X360CEManagerSettings Settings
         {
             get => settings;
-            set
-            {
-                settings = value;
-                OnPropertyChanged();
-            }
+            set { settings = value; OnPropertyChanged(); }
         }
 
         public X360CEManagerSettingsViewModel(X360CEManager plugin)
         {
-            // Injecting your plugin instance is required for Save/Load method because Playnite saves data to a location based on what plugin requested the operation.
+            // Injecting your plugin instance is required for Save/Load method because Playnite saves data
+            // to a location based on what plugin requested the operation.
             this.plugin = plugin;
 
             // Load saved settings.
             var savedSettings = plugin.LoadPluginSettings<X360CEManagerSettings>();
 
             // LoadPluginSettings returns null if no saved data is available.
-            if (savedSettings != null)
-            {
-                Settings = savedSettings;
-            }
-            else
-            {
-                Settings = new X360CEManagerSettings();
-            }
+            if (savedSettings != null) { Settings = savedSettings; }
+            else { Settings = new X360CEManagerSettings(); }
         }
 
-        public void BeginEdit()
-        {
-            // Code executed when settings view is opened and user starts editing values.
-            editingClone = Serialization.GetClone(Settings);
-        }
+        public void BeginEdit() { editingClone = Serialization.GetClone(Settings); }
 
-        public void CancelEdit()
-        {
-            // Code executed when user decides to cancel any changes made since BeginEdit was called.
-            // This method should revert any changes made to Option1 and Option2.
-            Settings = editingClone;
-        }
+        public void CancelEdit() { Settings = editingClone; }
 
-        public void EndEdit()
-        {
-            // Code executed when user decides to confirm changes made since BeginEdit was called.
-            // This method should save settings made to Option1 and Option2.
-            plugin.SavePluginSettings(Settings);
-        }
+        public void EndEdit() { plugin.SavePluginSettings(Settings); }
 
         public bool VerifySettings(out List<string> errors)
         {
-            // Code execute when user decides to confirm changes made since BeginEdit was called.
-            // Executed before EndEdit is called and EndEdit is not called if false is returned.
-            // List of errors is presented to user if verification fails.
             errors = new List<string>();
-            return true;
+            if (Settings.x360cePath.Equals(string.Empty)) { errors.Add("x360ce path not set"); }
+            return errors.Count == 0;
         }
     }
 }
